defc7c975639857e1e66fa2e9b2ff52663450d5f
valdis <>
2021-12-24 17:26:14 +0300

    work by tutorial

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -5,16 +5,37 @@ import './index.css';
 class Square extends React.Component {
     render() {
       return (
-        <button className="square">
-          {/* TODO */}
+        <button 
+          className="square"
+          onClick={() => {this.props.onClick()}}
+        >
+          {this.props.value === 1 ? "X" : this.props.value === 4 ? "0" : '' }
         </button>
       );
     }
   }
-  
+
   class Board extends React.Component {
+    constructor(props){
+      super(props);
+      this.state = {
+        squares: Array(9).fill(null),
+      };
+    }
+
+    handleClick(i){
+      const squares = this.state.squares.slice();
+      squares[i] = 1;
+      this.setState({squares: squares});
+    }
+
     renderSquare(i) {
-      return <Square />;
+      return (
+        <Square 
+          value={this.state.squares[i]} 
+          onClick={()=> this.handleClick(i)}
+        />
+      );
     }
   
     render() {
