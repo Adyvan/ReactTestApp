9f19e99d540deb022529d78d2919dcbec6b3aca6
valdis <>
2021-12-28 16:23:47 +0300

    show position of step

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -70,7 +70,7 @@ class Game extends React.Component {
     const current = history[history.length - 1];
     const squares = current.squares.slice();
     
-    if (!calculateWinner(squares) && !squares[i]) {
+    if (!calculateFinishGame(squares, history.length) && !squares[i]) {
       squares[i] = this.state.xIsNext ? XValue : OValue;
       this.setState({
         history: history.concat([{
@@ -85,7 +85,7 @@ class Game extends React.Component {
   render() {
     const history = this.state.history;
     const current = history[this.state.stepNumber];
-    const winner = calculateWinner(current.squares);
+    const winner = calculateFinishGame(current.squares, history.length);
     
     let status = 'Следующий ход: ' + (this.props.xIsNext ? 'X' : '0');
     if (winner) {
@@ -93,9 +93,9 @@ class Game extends React.Component {
     }
 
     const moves = history.map((step, move) => {
-      const desc = move ? 
-        'Перейти к ходу #' + move:
-        'К началу игры';
+      const desc = move 
+      ? 'Перейти к ходу #' + move + ' ' + getLastPosition(history, move)
+      : 'К началу игры';
         return (
         <li key={move}>
           <button onClick={() => this.jumpTo(move)}>{desc}</button>
@@ -127,7 +127,7 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-function calculateWinner(squares) {
+function calculateFinishGame(squares) {
   const lines = [
     [0, 1, 2],
     [3, 4, 5],
@@ -141,8 +141,27 @@ function calculateWinner(squares) {
   for (let i = 0; i < lines.length; i++) {
     const [a, b, c] = lines[i];
     if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a];
+      return { won: squares[a] };
     }
   }
   return null;
+}
+
+function getLastPosition(history, move)
+{
+  if(!move || move > history.length)
+  {
+    return null;
+  }
+  const previus = history[move - 1].squares.slice();
+  const current = history[move].squares.slice();
+  let index = 0;
+  for (let i = 0; i < previus.length; i++) {
+    if(previus[i] !== current[i])
+    {
+      index = i;
+      break;
+    }
+  }
+  return '('+ (Math.floor(index / 3) + 1) +','+ (index % 3 + 1) +')';
 }
